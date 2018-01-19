3b0070a51e6973c5f1bca8fca7e4118f268c215e
Julio3Cesar <bob-cesar@hotmail.com.br>
2018-01-19 12:05:51 -0200

    add star icon in favorited recipes

---
+++ spec/features/user_view_star_in_favortes_recipes_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+feature 'User view star in favorites recipes' do
+  scenario 'in show page, success' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    login_as user
+    visit recipe_path recipe
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).to have_css("img[alt='Star']")
+  end
+
+  scenario 'in home page, success' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    login_as user
+    visit root_path
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).to have_css("img[alt='Star']")
+  end
+
+  scenario 'in view all recipes page, success' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    login_as user
+    visit recipes_path
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).to have_css("img[alt='Star']")
+  end
+
+  scenario 'in show page, fail' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+
+    login_as user
+    visit recipe_path recipe
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).not_to have_css("img[alt='Star']")
+  end
+
+  scenario 'in home page, fail' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+
+    login_as user
+    visit root_path
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).not_to have_css("img[alt='Star']")
+  end
+
+  scenario 'in home page, not logged, fail' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    visit root_path
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).not_to have_css("img[alt='Star']")
+  end
+
+  scenario 'in home page, not logged, fail' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    visit recipe_path recipe
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).not_to have_css("img[alt='Star']")
+  end
+
+  scenario 'user not view in all favorites page' do 
+    user = create :user, email: 'aspergesme@gmail.com' 
+    recipe = create :recipe
+    Favorite.create user: user, recipe: recipe
+
+    login_as user
+    visit favorites_recipes_path
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).not_to have_css("img[alt='Star']")
+  end
+end
